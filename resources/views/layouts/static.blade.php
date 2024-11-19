8ca71e438f416a613b067eb889a631a2b23d592c
ann daramola <ann.daramola@gmail.com>
2024-11-19 14:56:55 -0800

    Update meta details

--- resources/views/layouts/static.blade.php
+++ resources/views/layouts/static.blade.php
@@ -8,6 +8,27 @@
 
     <title>{{ config('app.name', 'Catch Your Vision') }}</title>
 
+    <meta name="title" content="Catch Your Vision | A KABUVU Program" />
+    <meta name="description" content="Let’s find out what happens when you get clear about who you are and who you want to be." />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://catchyourvision.org/" />
+    <meta property="og:title" content="Catch Your Vision | A KABUVU Program" />
+    <meta property="og:description" content="Let’s find out what happens when you get clear about who you are and who you want to be." />
+    <meta property="og:image" content="https://catchyourvision.org/assets/images/catch-your-vision.png" />
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://catchyourvision.org/" />
+    <meta property="twitter:title" content="Catch Your Vision | A KABUVU Program" />
+    <meta property="twitter:description" content="Let’s find out what happens when you get clear about who you are and who you want to be." />
+    <meta property="twitter:image" content="https://catchyourvision.org/assets/images/catch-your-vision.png" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('/assets/images/cyv.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('/assets/images/cyv.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('/assets/images/cyv.png') }}">
+    <link rel="shortcut icon" href="{{ url('/assets/images/cyv.png') }}">
+    <meta name="msapplication-TileColor" content="#9385c9">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="color-scheme" content="light">
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=poppins:200,200i,300,300i,500,500i,600,600i" rel="stylesheet"/>
