8eb69cb894f1a94541e65ee7b34b7347d79f2b89
ann daramola <ann.daramola@gmail.com>
2024-11-19 14:01:46 -0800

    Add section headings

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
                     <img src="{{ url('/assets/images/logo.png') }}" alt="Catch Your Vision" />
                 </a>
                 <div class="my-4 w-full sm:w-3/5 md:w-4/5 sm:mx-auto">
-                    <h2 class="text-3xl sm:text-5xl font-serif mt-4 mb-8">Experience a future of your own making.</h2>
+                    <h2 class="text-3xl sm:text-5xl font-serif mt-4 mb-8">What happens when you get clear about who you are and who you want to be?</h2>
                     <p class="text-xl md:text-2xl">The Catch Your Vision workshops are designed to bring you and your future closer to each other, and uncover the power of your imagination, your word, and your vision.</p>
                     <ul class="mx-2 my-10 md:m-10 space-y-1 md:space-y-2 text-lg md:text-2xl [&_li]:font-bold [&_li]:text-orange-700/80">
                         <li>Saturday, December 7, 2024</li>
@@ -28,6 +28,7 @@
         </section>
         <section id="why" class="md:px-5 py-10 max-w-[1000px] mx-auto my-20 border-t-[1px] border-t-indigo-100 flex flex-col items-center md:flex-row">
             <div class="w-full md:w-3/5 flex flex-col text-lg space-y-3 md:px-4">
+                <h2 class="text-3xl sm:text-5xl font-serif mt-4 mb-8">Experience a future of your own making.</h2>
                 <p>These days, it seems everybody does a vision board. It’s a powerful way to take a snapshot of what you want your life to look at a certain point in the future.</p>
                 <p>But, here at KABUVU, we know the power of community. So we created these workshops to make the visioning process a dynamic, community-based experience.</p>
                 <p>For an hour and half, you get to let your imagination run wild and free, without judgement, and with a group of people cheering you on.</p>
@@ -44,6 +45,7 @@
         </section>
         <section id="what" class="md:px-5 py-10 max-w-[1000px] mx-auto my-20 border-t-[1px] border-t-indigo-100 flex flex-col items-center md:flex-row">
             <div class="w-full md:w-3/5 flex flex-col text-lg space-y-3 md:px-4 order-2">
+                <h2 class="text-3xl sm:text-5xl font-serif mt-4 mb-8">A workshop designed to bring your future closer.</h2>
                 <p>You’ll leave the workshop with a sense of direction, clarity, and hopefully some new friends.</p>
                 <p>You’ll also leave with a draft of your vision that you can turn into a vision board, a plan, or anything you need to see it come to pass.</p>
                 <p>Above all, this workshop is a powerful way to jumpstart any resolution you have for 2025.</p>
