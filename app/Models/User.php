429c4d6a03bf8bb0b9f557da62d35ee11c56c11b
ann daramola <ann.daramola@gmail.com>
2024-12-12 13:56:38 -0800

    Add FilamentUser contract to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Pariwo\Resources\Contracts\ResourceModelInterface;
 
-class User extends Authenticatable implements ResourceModelInterface
+class User extends Authenticatable implements ResourceModelInterface, FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
